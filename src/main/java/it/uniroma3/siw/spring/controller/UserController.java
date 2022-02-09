6b8747f4d8656298c774d7337040b79d8fa93dc0
Filippo <filipposcal@gmail.com>
2022-02-09 18:16:44 +0100

    front end updates

--- src/main/java/it/uniroma3/siw/spring/controller/UserController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/UserController.java
@@ -1,26 +1,45 @@
 package it.uniroma3.siw.spring.controller;
 
+import it.uniroma3.siw.spring.controller.validator.UserValidator;
+import it.uniroma3.siw.spring.model.Credentials;
+import it.uniroma3.siw.spring.model.User;
 import it.uniroma3.siw.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
 
+    @Autowired
     UserService userService;
 
-    @GetMapping (path="/admin/users/all")
-    public String getUsers(Model model){
-        model.addAttribute("users",this.userService.getAllUsers());
-        return "users";
+    @GetMapping(path = "/admin/users/all")
+    public String getUsers(Model model) {
+        model.addAttribute("users", this.userService.getAllUsers());
+        return "admin/allUsers";
     }
 
-    @GetMapping (path="/admin/user/{id}")
-    public String getUser(Model model, @PathVariable Long id){
-        model.addAttribute("user",this.userService.getUser(id));
+    @RequestMapping(value = "/admin/user/register", method = RequestMethod.GET)
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new User());
+        return "admin/newUser.html";
+    }
+
+    @RequestMapping(value = {"/admin/user/register"}, method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("user") User user,
+                               BindingResult userBindingResult) {
+        userService.saveUser(user);
+        return "admin/userSuccessful";
+    }
+
+    @PostMapping
+
+    @GetMapping(path = "/admin/user/{id}")
+    public String getUser(Model model, @PathVariable Long id) {
+        model.addAttribute("user", this.userService.getUser(id));
         return "user";
     }
 
